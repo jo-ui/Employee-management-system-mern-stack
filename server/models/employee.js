da5a3f33c0b7ebf60bb73bcd5957f58a81b9ca21
jo-ui <57154082+jo-ui@users.noreply.github.com>
2021-09-24 23:27:37 +0300

    Update employee.js

--- server/models/employee.js
+++ server/models/employee.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
 const employeeSchema = mongoose.Schema({
-    empId: Number,
-    employeeName: String,
-    birthDate: String,
-    gender: String,
-    salary: Number
+    empId: {
+        type: Number,
+        required:true,
+    },
+    employeeName: {
+        type: String,
+        required:true,
+    },
+    birthDate: {
+        type: String,
+        required:true,
+    },
+    gender: {
+        type: String,
+        required:true,
+    },
+    salary: {
+        type: Number,
+        required:true,
+    },
 });
 
 const employee = mongoose.model('employee', employeeSchema);
